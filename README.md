1e3c8098145cef02820e3e198223db70d4a5ca9d
K-Roon <66551757+K-Roon@users.noreply.github.com>
2025-01-13 19:56:34 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Food_recommend
+ 식당 추천 앱
---
+++ android/app/src/main/java/com/kroon/food_recommend/MainActivity.java
@@ -0,0 +1,6 @@
+package com.kroon.food_recommend;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
